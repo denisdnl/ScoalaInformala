b87b3cf4ff8704c784df2690e71e01ecf5865f6c
DIR\denis.daniel.stan <clone main repo on accenture machine>
2018-03-20 20:31:32 +0200

    catalogTemaV2

--- catalogTema/script.js
+++ catalogTema/script.js
@@ -1,7 +1,7 @@
 var Student={
     name:null,
     uid:null,
-    grades:[],
+    grades:null,
 
     getAverage:function(){
         if(this.grades.length == 0)
@@ -9,7 +9,7 @@ var Student={
         var s=0;
         for(var i=0;i<this.grades.length;i++)
             s+=this.grades[i];
-        return s/this.grades.length;
+        return (s/this.grades.length).toFixed(2);
     }
 };
 
@@ -56,6 +56,7 @@ function addStudent(){
     var s = Object.create(Student);
     s.name = st;
     s.uid = guid();
+    s.grades = [];
     studentsList.push(s);
 
     refreshStudentList();
@@ -63,8 +64,82 @@ function addStudent(){
 }
 
 
+function refreshGradesList(){
+    var gradesTable = document.getElementById("gradesList").querySelector("tbody");
+    var html = "";
+    var currentStudent = studentsList.filter(a=>{return a.uid === currentUID})[0];
+
+    for(var i=0;i<currentStudent.grades.length;i++){
+        var rand = `<tr><td>${currentStudent.grades[i]}</td></tr>`;
+        html+=rand;
+    }
+
+    gradesTable.innerHTML = html;
+}
+
+function refreshGradesStudentName(){
+    var currentStudent = studentsList.filter(a=>{return a.uid === currentUID})[0];
+    document.querySelector("#note_elevi_wrapper>p").innerHTML = `Note elev ${currentStudent.name}`;
+}
+
 function showGrades(i){
     document.getElementById("note_elevi_wrapper").style.display="block";
     currentUID = studentsList[i].uid;
+
+    refreshGradesStudentName();
     refreshGradesList();
+}
+
+
+function addGrade(){
+    
+    var grade = document.getElementById("addGradeTxt").value;
+
+    if(Number.parseInt(grade) <= 0 || Number.parseInt(grade) > 10 || grade.length == 0 || isNaN(grade)) 
+        return;
+
+    var currentStudent = studentsList.filter(a=>{return a.uid === currentUID})[0];
+    currentStudent.grades.push(Number.parseInt(grade));
+    refreshGradesList();
+    refreshStudentList();
+}
+
+
+function sortAscStudents(){
+    studentsList.sort((a,b)=>{return a.getAverage() >= b.getAverage()});
+    refreshStudentList();
+}
+
+
+function sortDescStudents(){
+    studentsList.sort((a,b)=>{return a.getAverage() < b.getAverage()});
+    refreshStudentList();
+}
+
+function sortAscGrades(){
+    studentsList.filter(a=>{return a.uid === currentUID})[0].grades.sort((a,b)=>{return a >= b});
+    refreshGradesList();
+}
+
+
+function sortDescGrades(){
+    studentsList.filter(a=>{return a.uid === currentUID})[0].grades.sort((a,b)=>{return a < b});
+    refreshGradesList();
+}
+
+
+function keyPressSt(ev){
+    if(ev.keyCode == 13){
+        ev.preventDefault();
+        addStudent();
+        document.getElementById("addStudentTxt").value = "";
+    }
+}
+
+function keyPressGrd(ev){
+    if(ev.keyCode == 13){
+        ev.preventDefault();
+        addGrade();
+        document.getElementById("addGradeTxt").value = "";
+    }
 }
