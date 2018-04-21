b6c8f592b0f9a35ea536dd26d60eb42ec55ba39e
DIR\denis.daniel.stan <clone main repo on accenture machine>
2018-04-21 13:19:21 +0300

    asdasfdsf

---
+++ testca/admin/script_template.js
@@ -0,0 +1,12 @@
+function hideLoadingModal() {
+  document.getElementById("loadingModal").classList = ["loadingModalHidden"];
+}
+
+function showLoadingModal() {
+  document.getElementById("loadingModal").classList = ["loadingModalShown"];
+}
+
+
+function login(){
+  window.location = "actiune.html";
+}
