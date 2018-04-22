3454fef33034b2ae6ff916eef55127ab5ca0d396
DIR\denis.daniel.stan <clone main repo on accenture machine>
2018-04-22 11:06:50 +0300

    asda

--- tst/prnou/admin/adminBox/script_template.js
+++ tst/prnou/admin/adminBox/script_template.js
@@ -52,7 +52,7 @@ function login(){
 }
 
 function draw() {
-
+  showLoadingModal();
   var http = new XMLHttpRequest();
 
   http.onload = function(response){
@@ -93,6 +93,8 @@ function drawList(){
       str += rand;
     }
     tabel.innerHTML = str;
+
+    hideLoadingModal();
 }
 
 function openEditor(i){
